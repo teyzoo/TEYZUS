edb823064faa2d87eb7cd91a60b7c9602ae94710
teyzoo <igorborisenko676@icloud.com>
2026-08-07 17:37:43 +0300

    Update main.py

--- bot/app/main.py
+++ bot/app/main.py
@@ -6,8 +6,11 @@ from app.config import settings
 
 from app.handlers.start import router as start_router
 from app.handlers.menu import router as menu_router
+
 from app.handlers.search_start import router as search_start_router
 from app.handlers.search_process import router as search_process_router
+
+from app.search_menu.handlers import router as search_menu_router
 
 
 bot = Bot(
@@ -18,11 +21,19 @@ bot = Bot(
 dp = Dispatcher()
 
 
+# Старт
 dp.include_router(
     start_router
 )
 
 
+# Меню поиска 🔎
+dp.include_router(
+    search_menu_router
+)
+
+
+# Старый поиск
 dp.include_router(
     search_start_router
 )
@@ -33,6 +44,7 @@ dp.include_router(
 )
 
 
+# Главное меню
 dp.include_router(
     menu_router
 )
@@ -40,7 +52,9 @@ dp.include_router(
 
 async def main():
 
-    print("TEYZUS Bot started")
+    print(
+        "🚀 TEYZUS Bot started"
+    )
 
     await dp.start_polling(
         bot
@@ -48,4 +62,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+
+    asyncio.run(
+        main()
+    )
