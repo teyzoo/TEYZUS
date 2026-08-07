eef57fe854fa0f9fe38a375dc4f5944f1becf4f8
teyzoo <igorborisenko676@icloud.com>
2026-08-07 18:09:14 +0300

    Create router.py

---
+++ backend/app/checker/router.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter
+
+from app.checker.service import (
+    check_username
+)
+
+
+router = APIRouter(
+    prefix="/checker",
+    tags=["Checker"]
+)
+
+
+
+@router.post("/")
+async def checker(
+    data: dict
+):
+
+    username = data.get(
+        "username"
+    )
+
+
+    if not username:
+
+        return {
+            "error": "username required"
+        }
+
+
+    result = await check_username(
+        username
+    )
+
+
+    return result
