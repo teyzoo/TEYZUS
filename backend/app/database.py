6d5871e6d28f471147e273d3fb2fb1592110e2c4
teyzoo <igorborisenko676@icloud.com>
2026-08-07 12:42:05 +0300

    Create database.py

---
+++ backend/app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.config import settings
+
+
+engine = create_engine(
+    settings.DATABASE_URL
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_database():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
