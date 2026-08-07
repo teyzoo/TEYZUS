e58cff568bf25a314b854f7033fc783c0ee95c41
teyzoo <igorborisenko676@icloud.com>
2026-08-07 12:48:14 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
 from app.config import settings
+from app.database import Base, engine
+
+from app.users.router import router as users_router
+
+
+Base.metadata.create_all(
+    bind=engine
+)
 
 
 app = FastAPI(
@@ -9,8 +17,14 @@ app = FastAPI(
 )
 
 
+app.include_router(
+    users_router
+)
+
+
 @app.get("/")
 def home():
+
     return {
         "project": "TEYZUS",
         "status": "online",
