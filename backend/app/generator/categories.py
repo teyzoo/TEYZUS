2600d9f6f8d6567fa192fb27c79760407c4205f0
teyzoo <igorborisenko676@icloud.com>
2026-08-07 17:41:32 +0300

    Create categories.py

---
+++ backend/app/generator/categories.py
@@ -0,0 +1,35 @@
+CATEGORIES = {
+
+    "ai": [
+        "ai",
+        "bot",
+        "neural",
+        "data",
+        "core"
+    ],
+
+
+    "future": [
+        "future",
+        "nova",
+        "vision",
+        "next"
+    ],
+
+
+    "business": [
+        "pay",
+        "trade",
+        "market",
+        "shop"
+    ],
+
+
+    "luxury": [
+        "gold",
+        "royal",
+        "elite",
+        "prime"
+    ]
+
+}
