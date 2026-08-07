3c7fcad52b77ab2d2845b3514def7a7c295015d3
teyzoo <igorborisenko676@icloud.com>
2026-08-07 19:11:15 +0300

    Update config.py

--- bot/app/config.py
+++ bot/app/config.py
@@ -1,10 +1,21 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Settings:
+
     BOT_TOKEN = os.getenv(
-        "BOT_TOKEN",
-        ""
+        "BOT_TOKEN"
+    )
+
+    OWNER_ID = int(
+        os.getenv(
+            "OWNER_ID",
+            "0"
+        )
     )
 
 
