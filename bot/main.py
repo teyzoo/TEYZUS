fcc296e44c61bdaf72e0f68c6e8144f988354ae4
teyzoo <igorborisenko676@icloud.com>
2026-08-07 19:09:50 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,9 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.config import settings
-
 from app.search_menu.handlers import router
-
 
 
 async def main():
@@ -16,25 +14,12 @@ async def main():
 
     dp = Dispatcher()
 
+    dp.include_router(router)
 
-    dp.include_router(
-        router
-    )
+    print("🚀 TEYZUS Bot started")
 
-
-    print(
-        "🚀 TEYZUS Bot started"
-    )
-
-
-    await dp.start_polling(
-        bot
-    )
-
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-
-    asyncio.run(
-        main()
-    )
+    asyncio.run(main())
