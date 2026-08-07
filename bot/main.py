89916478eba6c99ce5ae937f5a5d0813411e1c02
teyzoo <igorborisenko676@icloud.com>
2026-08-07 18:17:01 +0300

    Create main.py

---
+++ bot/main.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from app.config import settings
+
+from app.search_menu.handlers import router
+
+
+
+async def main():
+
+    bot = Bot(
+        token=settings.BOT_TOKEN
+    )
+
+    dp = Dispatcher()
+
+
+    dp.include_router(
+        router
+    )
+
+
+    await dp.start_polling(
+        bot
+    )
+
+
+
+if __name__ == "__main__":
+
+    asyncio.run(
+        main()
+    )
