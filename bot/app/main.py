fb1af8af5e8530ae063c13c48dd412534d3fb82e
teyzoo <igorborisenko676@icloud.com>
2026-08-07 17:12:18 +0300

    Update main.py

--- bot/app/main.py
+++ bot/app/main.py
@@ -3,18 +3,26 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.config import settings
-from app.handlers.start import router
+
+from app.handlers.start import router as start_router
+from app.handlers.menu import router as menu_router
 
 
 bot = Bot(
     token=settings.BOT_TOKEN
 )
 
+
 dp = Dispatcher()
 
 
 dp.include_router(
-    router
+    start_router
+)
+
+
+dp.include_router(
+    menu_router
 )
 
 
