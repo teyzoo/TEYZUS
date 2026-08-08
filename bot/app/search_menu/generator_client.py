16228add9d59a832dbe14f4c43c8dccd7a25e81e
teyzoo <igorborisenko676@icloud.com>
2026-08-08 05:46:53 +0300

    Update generator_client.py

--- bot/app/search_menu/generator_client.py
+++ bot/app/search_menu/generator_client.py
@@ -3,29 +3,25 @@ import aiohttp
 BACKEND_URL = os.getenv(
     "BACKEND_URL",
     "http://localhost:8000"
+).rstrip("/")
+TIMEOUT = aiohttp.ClientTimeout(
+    total=20
 )
 async def generate_usernames(
     length: int,
     numbers: bool
 ):
-    """
-    Запрашивает генерацию username у backend.
-    BACKEND_URL берётся из переменной окружения.
-    Локально по умолчанию используется localhost:8000.
-    """
     data = {
         "length": length,
         "numbers": numbers
     }
-    timeout = aiohttp.ClientTimeout(
-        total=30
-    )
     async with aiohttp.ClientSession(
-        timeout=timeout
+        timeout=TIMEOUT
     ) as session:
         async with session.post(
             f"{BACKEND_URL}/generator/",
             json=data
         ) as response:
-            response.raise_for_status()
+            if response.status >= 400:
+                return None
             return await response.json()
